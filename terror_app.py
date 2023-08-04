dbc0407b17ba3e6522974e45087f10db56e064cf
Adrian Florin <adrian_florin@dlsu.edu.ph>
2023-08-04 08:53:41 +0800

    added dropdown functionality

--- terror_app.py
+++ terror_app.py
@@ -4,16 +4,13 @@ import plotly.express as px
 import dash_bootstrap_components as dbc
 
 #Import data
-df = pd.read_csv('globalterrorismdb_0718dist.csv', encoding="ISO-8859-1", low_memory=False)
+df = pd.read_csv('https://media.githubusercontent.com/media/adrian-florin/datasets/main/terrorism_data.csv', encoding="ISO-8859-1", low_memory=False)
 df['count'] = 1
 years_attack = df['iyear'].unique()
 
-# years_options = {}
-# for i in years_attack:
-#     year = int(i) 
-#     years_options[year] = '{}'.format(year)
+years_options = {1970: '1970', 1972: '1972', 1975: '1975', 1980: '1980', 1985: '1985', 1990: '1990', 1995: '1995', 2000: '2000', 2005: '2005', 2010: '2010', 2014: '2014', 2017: '2017'}
 
-years_options = {1970: '1970', 1972: '1972', 1975: '1975', 1980: '1980', 1985: '1985', 1990: '1990', 1995: '1995', 2000: '2000', 2005: '2005', 2010: '2010', 2015: '2015', 2017: '2017'}
+regions = df['region_txt'].unique()
 
 #Mapbox Token
 px.set_mapbox_access_token(open(".mapbox_token").read())
@@ -24,38 +21,32 @@ value_two = 'count'
 grp = 'gname'
 region = 'Middle East & North Africa'
 
+# options
+region_option = df['region_txt'].unique()
+grp_option = ['gname', 'attacktype1_txt', 'targtype1_txt', 'weaptype1_txt']
+
 #Data Transformation
-df_region = df.loc[df['region_txt'] == region].reset_index(drop=True)
+df_region = df.loc[df['region_txt'] == region_option[0]].reset_index(drop=True)
 
-# Scatter Map Data
-df_cam = df[df['region_txt'] == 'Central America & Caribbean'].reset_index(drop=True)
-df_north_america = df[df['region_txt'] == 'North America'].reset_index(drop=True)
-df_sea = df[df['region_txt'] == 'Southeast Asia'].reset_index(drop=True)
-df_we = df[df['region_txt'] == 'Western Europe'].reset_index(drop=True)
-df_ea = df[df['region_txt'] == 'East Asia'].reset_index(drop=True)
-df_sam = df[df['region_txt'] == 'South America'].reset_index(drop=True)
-df_ee= df[df['region_txt'] == 'Eastern Europe'].reset_index(drop=True)
-df_ssa = df[df['region_txt'] == 'Sub-Saharan Africa'].reset_index(drop=True)
-df_mena = df[df['region_txt'] == 'Middle East & North Africa'].reset_index(drop=True)
-df_ao = df[df['region_txt'] == 'Australasia & Oceania'].reset_index(drop=True)
-df_sa = df[df['region_txt'] == 'South Asia'].reset_index(drop=True)
-df_ca = df[df['region_txt'] == 'Central Asia'].reset_index(drop=True)
+# scatter map data
+scatter_dict_region = {}
+for i in region_option:
+    df_selected_region = df[df['region_txt'] == i].reset_index(drop=True)
+    scatter_dict_region = {**scatter_dict_region, i : df_selected_region}
 
-df_cam_top10 = df_cam.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-df_north_america_top10 = df_north_america.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-df_sea_top10 = df_sea.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-df_we_top10 = df_we.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-df_ea_top10 = df_ea.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-df_sam_top10 = df_sam.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-df_ee_top10 = df_ee.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-df_ssa_top10 = df_ssa.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-df_mena_top10 = df_mena.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-df_ao_top10 = df_ao.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-df_sa_top10 = df_sa.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-df_ca_top10 = df_ca.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
+scatter_dict_top_group = {}
+for i in scatter_dict_region:
+    df_selected_group = scatter_dict_region.get(i)
+    df_selected_group = df_selected_group.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
+    name_df = i + " Top 10"
+    scatter_dict_top_group = {**scatter_dict_top_group, name_df : df_selected_group}
 
-top_10_groups_per_region = pd.concat([df_cam_top10, df_north_america_top10, df_sea_top10, df_we_top10, df_ea_top10,
-                                      df_sam_top10, df_ee_top10, df_ssa_top10, df_mena_top10, df_ao_top10, df_sa_top10, df_ca_top10])
+top_10_groups_list = []
+for i in scatter_dict_top_group:
+    selected_df = scatter_dict_top_group.get(i)
+    top_10_groups_list.append(selected_df)
+
+top_10_groups_per_region = pd.concat(top_10_groups_list)
 dropped = ['attack_count', 'region_txt']
 top_10_groups_per_region = top_10_groups_per_region.drop(dropped, axis=1).drop_duplicates().reset_index(drop=True)
 
@@ -67,7 +58,7 @@ top_10_groups_all_regions = df[df['gname'].isin(top_10_groups_per_region_list)]
 df_hor_bar = df_region.groupby(['country_txt'])[[value_one, value_two]].sum().reset_index()
 df_hor_bar = df_hor_bar.sort_values(by=value_one, ascending=True).tail(5)
 
-# Line Graph Data
+# Line Chat Data
 df_pie = pd.DataFrame(df_region.groupby(grp)[value_one].sum()).reset_index()
 df_pie = df_pie.sort_values(by=value_one, ascending=False).head(9)
 
@@ -97,6 +88,8 @@ df_stacked_pivot = df_stacked_pivot.reset_index()
 df_stacked_pivot = df_stacked_pivot[top_group]
 
 #Plotly Express Figures
+
+## big map
 fig_map = px.scatter_mapbox(top_10_groups_all_regions,
                         lat='latitude',
                         lon='longitude',
@@ -105,6 +98,16 @@ fig_map = px.scatter_mapbox(top_10_groups_all_regions,
                         zoom=1)
 fig_map.update_layout(showlegend=False)
 
+## small map
+fig_map_small = px.scatter_mapbox(top_10_groups_all_regions,
+                        lat='latitude',
+                        lon='longitude',
+                        hover_name="city",
+                        color=grp,
+                        zoom=2)
+fig_map_small.update_layout(height=350, width=450, margin={"r":0,"t":0,"l":0,"b":0}, showlegend=False)
+
+## grouped bar
 fig_bar = px.bar(df_hor_bar, x=[value_one, value_two], y='country_txt', orientation='h', barmode='group')
 fig_bar.update_layout(
     width=400,
@@ -127,6 +130,7 @@ fig_bar.update_layout(
     )
 )
 
+## pie chart
 fig_pie = px.pie(df_pie, values=value_one, names=grp)
 fig_pie.update_layout(showlegend=False,
     width=250,
@@ -149,7 +153,7 @@ fig_pie.update_layout(showlegend=False,
     )
 )
 
-
+## line chart
 fig_line = px.area(df_time, x='iyear', y=value_one, color=grp)
 fig_line.update_layout(
     width=400,
@@ -172,6 +176,7 @@ fig_line.update_layout(
     )
 )
 
+## stacked bar chart
 fig_stacked = px.bar(df_stacked_pivot.reset_index(), x='country_txt', y=df_stacked_pivot.columns)
 fig_stacked.update_layout(
     width=400,
@@ -218,6 +223,13 @@ navbar = dbc.NavbarSimple(
     dark=True,
 )
 
+region_options = []
+for i in regions:
+    region_options.append({
+        'label': i, 
+        'value': i
+    })
+
 # App layout
 app.layout = html.Div(children=[
     navbar,
@@ -237,12 +249,15 @@ app.layout = html.Div(children=[
         ]),
         dbc.Row(children=[
              dbc.Col(children=[
-                
+                dcc.Graph(id="scatter-map-small", figure=fig_map_small)
             ], width=4),
             dbc.Col(children=[
                 dcc.Graph(id="bar-chart", figure=fig_bar)
             ], width=4),
             dbc.Col(children=[
+                html.Div(children=[dcc.Dropdown(id="region-dropdown", 
+                                                options=region_options, value=None)], 
+                                                className='mb-2'),
                 dcc.Graph(id="pie-chart", figure=fig_pie)
             ], width=4),
         ]),
@@ -251,70 +266,169 @@ app.layout = html.Div(children=[
                 dcc.Graph(id="line-chart", figure=fig_line)
             ], width=4),
             dbc.Col(children=[
+                dcc.Dropdown(id="stacked-dropdown", options=grp_option),
                 dcc.Graph(id="stacked-chart", figure=fig_stacked)
             ], width=4),
-            # dbc.Col(children=[
-                
-            # ], width=4),
         ])
     ])
 ])    
 
+# big map callback
 @callback(
     Output('scatter-map', 'figure'),
     Input('birth-year-slider', 'value')
 )
 
 def update_attack_map(selected_year):
+
+    scatter_dict_region = {}
+    for i in region_option:
+        df_selected_region = df[df['region_txt'] == i].reset_index(drop=True)
+        scatter_dict_region = {**scatter_dict_region, i : df_selected_region}
+
+    scatter_dict_top_group = {}
+    for i in scatter_dict_region:
+        df_selected_group = scatter_dict_region.get(i)
+        df_selected_group = df_selected_group.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
+        name_df = i + " Top 10"
+        scatter_dict_top_group = {**scatter_dict_top_group, name_df : df_selected_group}
+
+    top_10_groups_list = []
+    for i in scatter_dict_top_group:
+        selected_df = scatter_dict_top_group.get(i)
+        top_10_groups_list.append(selected_df)
+
+    top_10_groups_per_region = pd.concat(top_10_groups_list)
+    dropped = ['attack_count', 'region_txt']
+    top_10_groups_per_region = top_10_groups_per_region.drop(dropped, axis=1).drop_duplicates().reset_index(drop=True)
+
+    top_10_groups_per_region_list = top_10_groups_per_region['gname'].tolist()
+    top_10_groups_all_regions = df[df['gname'].isin(top_10_groups_per_region_list)]
+        
     if selected_year is None:
-        pass
+        top_10_groups_all_regions = top_10_groups_all_regions[top_10_groups_all_regions['iyear'] == 1970]
 
     else:
-        df_cam = df[df['region_txt'] == 'Central America & Caribbean'].reset_index(drop=True)
-        df_north_america = df[df['region_txt'] == 'North America'].reset_index(drop=True)
-        df_sea = df[df['region_txt'] == 'Southeast Asia'].reset_index(drop=True)
-        df_we = df[df['region_txt'] == 'Western Europe'].reset_index(drop=True)
-        df_ea = df[df['region_txt'] == 'East Asia'].reset_index(drop=True)
-        df_sam = df[df['region_txt'] == 'South America'].reset_index(drop=True)
-        df_ee= df[df['region_txt'] == 'Eastern Europe'].reset_index(drop=True)
-        df_ssa = df[df['region_txt'] == 'Sub-Saharan Africa'].reset_index(drop=True)
-        df_mena = df[df['region_txt'] == 'Middle East & North Africa'].reset_index(drop=True)
-        df_ao = df[df['region_txt'] == 'Australasia & Oceania'].reset_index(drop=True)
-        df_sa = df[df['region_txt'] == 'South Asia'].reset_index(drop=True)
-        df_ca = df[df['region_txt'] == 'Central Asia'].reset_index(drop=True)
-
-        df_cam_top10 = df_cam.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-        df_north_america_top10 = df_north_america.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-        df_sea_top10 = df_sea.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-        df_we_top10 = df_we.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-        df_ea_top10 = df_ea.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-        df_sam_top10 = df_sam.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-        df_ee_top10 = df_ee.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-        df_ssa_top10 = df_ssa.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-        df_mena_top10 = df_mena.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-        df_ao_top10 = df_ao.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-        df_sa_top10 = df_sa.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-        df_ca_top10 = df_ca.groupby(['region_txt', 'gname', ]).size().reset_index(name='attack_count').sort_values(by=['region_txt', 'attack_count'], ascending=[True, False]).head(5)
-
-        top_10_groups_per_region = pd.concat([df_cam_top10, df_north_america_top10, df_sea_top10, df_we_top10, df_ea_top10,
-                                            df_sam_top10, df_ee_top10, df_ssa_top10, df_mena_top10, df_ao_top10, df_sa_top10, df_ca_top10])
-        dropped = ['attack_count', 'region_txt']
-        top_10_groups_per_region = top_10_groups_per_region.drop(dropped, axis=1).drop_duplicates().reset_index(drop=True)
-
-        top_10_groups_per_region_list = top_10_groups_per_region['gname'].tolist()
-        top_10_groups_all_regions = df[df['gname'].isin(top_10_groups_per_region_list)]
-
         top_10_groups_all_regions = top_10_groups_all_regions[top_10_groups_all_regions['iyear'] == selected_year]
 
-        fig_map = px.scatter_mapbox(top_10_groups_all_regions,
-                            lat='latitude',
-                            lon='longitude',
-                            hover_name="city",
-                            color=grp,
-                            zoom=1)
-        fig_map.update_layout(showlegend=False)
+    fig_map = px.scatter_mapbox(top_10_groups_all_regions,
+                        lat='latitude',
+                        lon='longitude',
+                        hover_name="city",
+                        color=grp,
+                        zoom=1)
+    fig_map.update_layout(showlegend=False)
 
-        return fig_map
+    return fig_map
+
+# small map callback
+@callback(
+    Output('scatter-map-small', 'figure'),
+    Input('region-dropdown', 'value')
+)
+
+def display_small_map(selected_region):
+    region_to_map = top_10_groups_all_regions[top_10_groups_all_regions['region_txt'] == selected_region]
+
+    fig_map_small = px.scatter_mapbox(region_to_map,
+                        lat='latitude',
+                        lon='longitude',
+                        hover_name="city",
+                        color=grp,
+                        zoom=2)
+    fig_map_small.update_layout(height=350, width=450, margin={"r":0,"t":0,"l":0,"b":0}, showlegend=False)
+
+    return fig_map_small
+
+# grouped bar callback
+@callback(
+    Output('stacked-chart', 'figure'),
+    Output('pie-chart', 'figure'),
+    Input('stacked-dropdown', 'value')
+)
+
+def update_stacked_and_pie(selected_group):
+
+    # updated pie data
+    df_pie = pd.DataFrame(df_region.groupby(selected_group)[value_one].sum()).reset_index()
+    df_pie = df_pie.sort_values(by=value_one, ascending=False).head(9)
+
+    # updated stacked data
+    df_stacked = pd.DataFrame(df_region.groupby(['country_txt', selected_group])[value_one].sum())
+    df_stacked = df_stacked.sort_values(by=[value_one])
+    df_stacked = df_stacked.reset_index()
+
+    top_group = df_pie[selected_group].unique().tolist()
+    top_group.append('country_txt')
+    df_stacked_pivot = pd.pivot_table(data=df_stacked,
+                                      index='country_txt',
+                                      columns=selected_group,
+                                      values=value_one)
+    df_stacked_pivot['total'] = df_stacked_pivot.sum(axis=1)
+    df_stacked_pivot = df_stacked_pivot.sort_values(by=['total'], ascending=False).head(10)
+    df_stacked_pivot = df_stacked_pivot.reset_index()
+    df_stacked_pivot = df_stacked_pivot[top_group]
+
+    # updated pie visualization
+    fig_pie = px.pie(df_pie, values=value_one, names=selected_group)
+
+    # updated stacked visualization
+    fig_stacked = px.bar(df_stacked_pivot.reset_index(), x='country_txt', y=df_stacked_pivot.columns)
+    fig_stacked.update_layout(width=400, 
+                              height=350, 
+                              margin=dict(l=0, r=0, b=0, t=0, pad=3), 
+                              legend=dict(yanchor="top", y=0.99, xanchor="right", x=0.99), 
+                              font=dict(size=9,))
+    
+    
+    return fig_stacked, fig_pie
+
+# line chart callback
+@callback(
+    Output("line-chart", 'figure'),
+    Input("stacked-dropdown", 'value')
+)
+
+def update_line_chart(selected_group):
+
+    # updated data
+    df_pie = pd.DataFrame(df_region.groupby(selected_group)[value_one].sum()).reset_index()
+    df_pie = df_pie.sort_values(by=value_one, ascending=False).head(9)
+
+    df_time = pd.DataFrame(df_region.groupby([selected_group, 'iyear'], as_index=False)[value_one].sum())
+    df_time_sorted = df_time.sort_values(by=value_one, ascending=False)
+    top_five_kills = df_time_sorted[selected_group].unique()[:5].tolist()
+    df_time = df_time.loc[(df_time[selected_group] == top_five_kills[0]) | 
+                        (df_time[selected_group] == top_five_kills[1]) | 
+                        (df_time[selected_group] == top_five_kills[2]) | 
+                        (df_time[selected_group] == top_five_kills[3]) | 
+                        (df_time[selected_group] == top_five_kills[4])]
+    
+    # updated chart
+    fig_line = px.area(df_time, x='iyear', y=value_one, color=selected_group)
+    fig_line.update_layout(
+        width=400,
+        height=350,
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0,
+            pad=3
+        ),
+        legend=dict(
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01
+        ),
+        font=dict(
+                size=9,
+        )
+    )
+
+    return fig_line
+
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
